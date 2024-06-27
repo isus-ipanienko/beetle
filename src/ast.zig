e9ac5594a557899d90e5ab20fe07844b5808d5f3
isus-ipanienko <preliga@proton.me>
2024-06-27 22:28:07 +0200

    Working on Pratt parsing

--- src/ast.zig
+++ src/ast.zig
@@ -2,30 +2,78 @@ const std = @import("std");
 
 const tok = @import("tokenizer.zig");
 
-pub const Identifier = struct {
-    token: tok.Token,
-};
-
 pub const Expression = union(enum) {
-    dummy_expression: DummyExpression,
+    identifier_expression: IdentifierExpression,
+    number_literal_expression: NumberLiteralExpression,
+    prefix_expression: PrefixExpression,
+    infix_expression: InfixExpression,
 
-    fn eval(self: Expression) void {
+    pub fn create(allocator: std.mem.Allocator, options: Expression) *Expression {
+        const new: *Expression = allocator.create(Expression) catch {
+            unreachable;
+        };
+        new.* = options;
+        return new;
+    }
+
+    fn destroy(self: *Expression, allocator: std.mem.Allocator) void {
         switch (self) {
+            inline else => |s| s.destroy(allocator),
+        }
+        allocator.destroy(self);
+    }
+
+    fn eval(self: *Expression) void {
+        switch (self.*) {
             inline else => |s| s.eval(),
         }
     }
 };
 
-const stdout = std.io.getStdOut().writer();
-pub const DummyExpression = struct {
-    fn eval(_: DummyExpression) void {
-        stdout.writeAll("Evaluated!\n") catch {};
+pub const PrefixExpression = struct {
+    operator: tok.TokenType,
+    expression: *Expression,
+
+    fn destroy(self: *PrefixExpression, allocator: std.mem.Allocator) void {
+        self.expression.destroy(allocator);
     }
+
+    fn eval(_: PrefixExpression) void {}
+};
+
+pub const InfixExpression = struct {
+    operator: tok.TokenType,
+    left: *Expression,
+    right: *Expression,
+
+    fn destroy(self: *InfixExpression, allocator: std.mem.Allocator) void {
+        self.left.destroy(allocator);
+        self.right.destroy(allocator);
+    }
+
+    fn eval(_: InfixExpression) void {}
+};
+
+pub const IdentifierExpression = struct {
+    token: tok.Token,
+
+    fn destroy(_: *IdentifierExpression, _: std.mem.Allocator) void {}
+
+    fn eval(_: IdentifierExpression) void {}
+};
+
+pub const NumberLiteralExpression = struct {
+    value: f64,
+
+    fn destroy(_: *NumberLiteralExpression, _: std.mem.Allocator) void {}
+
+    fn eval(_: NumberLiteralExpression) void {}
 };
 
 pub const Statement = union(enum) {
     var_statement: VarStatement,
     return_statement: ReturnStatement,
+    expression_statement: ExpressionStatement,
 
     fn execute(self: *Statement) void {
         switch (self.*) {
@@ -35,7 +83,7 @@ pub const Statement = union(enum) {
 };
 
 pub const ReturnStatement = struct {
-    value: Expression,
+    value: *Expression,
 
     fn execute(self: *ReturnStatement) void {
         self.value.eval();
@@ -43,10 +91,18 @@ pub const ReturnStatement = struct {
 };
 
 pub const VarStatement = struct {
-    identifier: Identifier,
-    value: Expression,
+    identifier: IdentifierExpression,
+    value: *Expression,
 
     fn execute(self: *VarStatement) void {
+        self.value.eval();
+    }
+};
+
+pub const ExpressionStatement = struct {
+    value: *Expression,
+
+    fn execute(self: *ExpressionStatement) void {
         self.value.eval();
     }
 };
--- src/parser.zig
+++ src/parser.zig
@@ -5,6 +5,7 @@ const ast = @import("ast.zig");
 
 pub const Error = union(enum) {
     token_error: TokenError,
+    prefix_error: PrefixError,
 
     pub fn toString(self: *Error, allocator: std.mem.Allocator) ![]u8 {
         return switch (self.*) {
@@ -26,12 +27,62 @@ const TokenError = struct {
     }
 };
 
+const PrefixError = struct {
+    token_type: tok.TokenType,
+
+    fn toString(self: PrefixError, allocator: std.mem.Allocator) ![]u8 {
+        return std.fmt.allocPrint(
+            allocator,
+            "prefix not defined for {s}",
+            .{@tagName(self.token_type)},
+        );
+    }
+};
+
 pub const Parser = struct {
     tokenizer: tok.Tokenizer,
     current_token: tok.Token,
     next_token: tok.Token,
     allocator: std.mem.Allocator,
     errors: std.ArrayList(Error),
+
+    const Precedence = enum(u8) {
+        LOWEST,
+        ASSIGN, // =
+        OR, // or
+        AND, // and
+        EQUALITY, // == !=
+        COMPARISON, // < > <= >=
+        SUM, // + -
+        PRODUCT, // * /
+        PREFIX, // ! -
+        CALL, // . ()
+    };
+
+    const ParseRule = struct {
+        prefix: ?*const fn (*Parser) ?*ast.Expression,
+        infix: ?*const fn (*Parser, *ast.Expression) ?*ast.Expression,
+        precedence: Precedence,
+    };
+
+    pub fn getParseRule(token_type: tok.TokenType) ParseRule {
+        comptime var callbacks =
+            [_]ParseRule{.{ .prefix = null, .infix = null, .precedence = .LOWEST }} ** 256;
+        callbacks[@intFromEnum(tok.TokenType.IDENTIFIER)] = .{ .prefix = parseIdentifierExpression, .infix = null, .precedence = .LOWEST };
+        callbacks[@intFromEnum(tok.TokenType.NUMBER)] = .{ .prefix = parseNumberLiteralExpression, .infix = null, .precedence = .LOWEST };
+        callbacks[@intFromEnum(tok.TokenType.BANG)] = .{ .prefix = parsePrefixExpression, .infix = null, .precedence = .LOWEST };
+        callbacks[@intFromEnum(tok.TokenType.MINUS)] = .{ .prefix = parsePrefixExpression, .infix = parseInfixExpression, .precedence = .SUM };
+        callbacks[@intFromEnum(tok.TokenType.PLUS)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .SUM };
+        callbacks[@intFromEnum(tok.TokenType.SLASH)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .PRODUCT };
+        callbacks[@intFromEnum(tok.TokenType.STAR)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .PRODUCT };
+        callbacks[@intFromEnum(tok.TokenType.EQUAL_EQUAL)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .EQUALITY };
+        callbacks[@intFromEnum(tok.TokenType.BANG_EQUAL)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .EQUALITY };
+        callbacks[@intFromEnum(tok.TokenType.LESS)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .COMPARISON };
+        callbacks[@intFromEnum(tok.TokenType.LESS_EQUAL)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .COMPARISON };
+        callbacks[@intFromEnum(tok.TokenType.GREATER)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .COMPARISON };
+        callbacks[@intFromEnum(tok.TokenType.GREATER_EQUAL)] = .{ .prefix = null, .infix = parseInfixExpression, .precedence = .COMPARISON };
+        return callbacks[@intFromEnum(token_type)];
+    }
 
     pub fn init(allocator: std.mem.Allocator, tokenizer: tok.Tokenizer) Parser {
         var parser = Parser{
@@ -63,7 +114,7 @@ pub const Parser = struct {
         return self.current_token.token_type == token_type;
     }
 
-    fn expectCurrentToken(self: *Parser, token_type: tok.TokenType) bool {
+    fn consumeToken(self: *Parser, token_type: tok.TokenType) bool {
         if (!self.isCurrentToken(token_type)) {
             return false;
         }
@@ -71,35 +122,9 @@ pub const Parser = struct {
         return true;
     }
 
-    const Precedence = enum(u8) {
-        LOWEST,
-        ASSIGN, // =
-        OR, // or
-        AND, // and
-        EQUALITT, // == !=
-        COMPARISON, // < > <= >=
-        SUM, // + -
-        PRODUCT, // * /
-        PREFIX, // ! -
-        CALL, // . ()
-    };
-
-    const OperatorCallbacks = struct {
-        prefix: fn () ast.Expression,
-        infix: fn (ast.Expression) ast.Expression,
-    };
-
-    fn parseExpression(self: *Parser, _: Precedence) ?ast.Expression {
-        // TODO: parse expressions
-        while (!self.isCurrentToken(.SEMICOLON)) {
-            self.advanceToken();
-        }
-        return .{ .dummy_expression = ast.DummyExpression{} };
-    }
-
-    fn parseIdentifier(self: *Parser) ?ast.Identifier {
+    fn parseIdentifier(self: *Parser) ?ast.IdentifierExpression {
         const current_token = self.current_token;
-        if (!self.expectCurrentToken(.IDENTIFIER)) {
+        if (!self.consumeToken(.IDENTIFIER)) {
             self.addError(.{ .token_error = .{
                 .actual = self.current_token,
                 .expected = "an identifier",
@@ -109,12 +134,84 @@ pub const Parser = struct {
         return .{ .token = current_token };
     }
 
+    fn parseIdentifierExpression(self: *Parser) ?*ast.Expression {
+        const identifier = self.parseIdentifier();
+        if (identifier == null) {
+            return null;
+        }
+        return ast.Expression.create(
+            self.allocator,
+            .{ .identifier_expression = identifier.? },
+        );
+    }
+
+    fn parseNumberLiteralExpression(self: *Parser) ?*ast.Expression {
+        if (!self.isCurrentToken(.NUMBER)) {
+            return null;
+        }
+        const number: f64 = std.fmt.parseFloat(f64, self.current_token.literal) catch {
+            return null;
+        };
+        self.advanceToken();
+        return ast.Expression.create(
+            self.allocator,
+            .{ .number_literal_expression = .{ .value = number } },
+        );
+    }
+
+    fn parsePrefixExpression(self: *Parser) ?*ast.Expression {
+        const operator = self.current_token.token_type;
+        self.advanceToken();
+        const expression = self.parseExpression(.PREFIX);
+        if (expression == null) {
+            return null;
+        }
+        return ast.Expression.create(
+            self.allocator,
+            .{ .prefix_expression = .{ .operator = operator, .expression = expression.? } },
+        );
+    }
+
+    fn parseInfixExpression(self: *Parser, left: *ast.Expression) ?*ast.Expression {
+        const operator = self.current_token.token_type;
+        const rule: ParseRule = getParseRule(operator);
+        self.advanceToken();
+        const right = self.parseExpression(rule.precedence);
+        if (right == null) {
+            return null;
+        }
+        return ast.Expression.create(
+            self.allocator,
+            .{ .infix_expression = .{ .operator = operator, .left = left, .right = right.? } },
+        );
+    }
+
+    fn parseExpression(self: *Parser, precedence: Precedence) ?*ast.Expression {
+        var rule: ParseRule = getParseRule(self.current_token.token_type);
+        if (rule.prefix == null) {
+            self.addError(.{ .prefix_error = .{ .token_type = self.current_token.token_type } });
+            return null;
+        }
+        var left = rule.prefix.?(self);
+        while (!self.isNextToken(.SEMICOLON) and
+            @intFromEnum(precedence) < @intFromEnum(getParseRule(self.next_token.token_type).precedence))
+        {
+            rule = getParseRule(self.current_token.token_type);
+            if (rule.infix == null or left == null) {
+                return left;
+            }
+            self.advanceToken();
+            left = rule.infix.?(self, left.?);
+        }
+        return left;
+    }
+
     fn parseVarStatement(self: *Parser) ?ast.VarStatement {
         const identifier = self.parseIdentifier();
         if (identifier == null) {
             return null;
         }
-        if (!self.expectCurrentToken(.EQUAL)) {
+        if (!self.consumeToken(.EQUAL)) {
             self.addError(.{ .token_error = .{
                 .actual = self.current_token,
                 .expected = "=",
@@ -136,6 +233,17 @@ pub const Parser = struct {
         return .{ .value = value.? };
     }
 
+    fn parseExpressionStatement(self: *Parser) ?ast.ExpressionStatement {
+        const value = self.parseExpression(.LOWEST);
+        if (value == null) {
+            return null;
+        }
+        if (self.isNextToken(.SEMICOLON)) {
+            self.advanceToken();
+        }
+        return .{ .value = value.? };
+    }
+
     fn parseStatement(self: *Parser) ?ast.Statement {
         switch (self.current_token.token_type) {
             .VAR => {
@@ -153,7 +261,11 @@ pub const Parser = struct {
             .IF => {},
             .FOR => {},
             .WHILE => {},
-            else => {},
+            else => {
+                if (self.parseExpressionStatement()) |s| {
+                    return .{ .expression_statement = s };
+                }
+            },
         }
         self.addError(.{ .token_error = .{
             .actual = self.current_token,
